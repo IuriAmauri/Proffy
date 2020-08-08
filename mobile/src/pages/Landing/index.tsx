2c08ac9ae4fdb5595a4a1a3e863376cece7b95f7
IuriAmauri <iuri_amauri@hotmail.com>
2020-08-08 13:01:19 -0300

    Added Landing page and StackNavigation

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+import styles from './styles';
+
+function Landing() {
+    return (
+        <View style={ styles.container }>
+            <Image source={ landingImg } style={ styles.banner }/>
+            <Text style={ styles.title }>
+                Seja bem-vindo, {'\n'}
+                <Text style={ styles.titleBold }>O quer deseja fazer?</Text>
+            </Text>
+            <View style={ styles.buttonsContainer }>
+                <TouchableOpacity style={ [styles.button, styles.buttonPrimary] }>
+                    <Image source={ studyIcon }/>
+                    <Text style={ styles.buttonText }>Estudar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={ [styles.button, styles.buttonSecondary] }>
+                    <Image source={ giveClassesIcon }/>
+                    <Text style={ styles.buttonText }>Dar Aulas</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={ styles.totalConnections }>
+                Total de 285 conexões realizadas {' '}
+                <Image source={heartIcon}></Image>
+            </Text>
+        </View>
+    );
+}
+
+export default Landing;
---
+++ mobile/src/routes/AppStack.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import Landing from '../pages/Landing';
+
+const { Navigator, Screen } = createStackNavigator();
+ function AppStack() {
+     return (
+         <NavigationContainer>
+             <Navigator>
+                 <Screen name="Landing" component={ Landing }/>
+             </Navigator>
+         </NavigationContainer>
+     );     
+ }
+
+ export default AppStack;

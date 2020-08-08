7a9624d0be1e5cadb884fa131113c82c52673689
IuriAmauri <iuri_amauri@hotmail.com>
2020-08-08 17:09:26 -0300

    Added PageHeader
    Added navigations (stack and tabs)

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -8,6 +10,16 @@ import heartIcon from '../../assets/images/icons/heart.png';
 import styles from './styles';
 
 function Landing() {
+    const { navigate } = useNavigation();
+
+    function handleNavigationToGiveClassesPage() {
+        navigate('GiveClasses');
+    }
+
+    function handleNavigateToStudyPage() {
+        navigate('Study');
+    }
+
     return (
         <View style={ styles.container }>
             <Image source={ landingImg } style={ styles.banner }/>
@@ -16,14 +28,20 @@ function Landing() {
                 <Text style={ styles.titleBold }>O quer deseja fazer?</Text>
             </Text>
             <View style={ styles.buttonsContainer }>
-                <TouchableOpacity style={ [styles.button, styles.buttonPrimary] }>
+                <RectButton 
+                    onPress={handleNavigateToStudyPage}
+                    style={ [styles.button, styles.buttonPrimary] }>
+                        
                     <Image source={ studyIcon }/>
                     <Text style={ styles.buttonText }>Estudar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={ [styles.button, styles.buttonSecondary] }>
+                </RectButton>
+                <RectButton 
+                    onPress={handleNavigationToGiveClassesPage} 
+                    style={ [styles.button, styles.buttonSecondary] }>
+
                     <Image source={ giveClassesIcon }/>
                     <Text style={ styles.buttonText }>Dar Aulas</Text>
-                </TouchableOpacity>
+                </RectButton>
             </View>
             <Text style={ styles.totalConnections }>
                 Total de 285 conexões realizadas {' '}
--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
+import StudyTabs from './StudyTabs';
 
 const { Navigator, Screen } = createStackNavigator();
  function AppStack() {
      return (
          <NavigationContainer>
-             <Navigator>
+             <Navigator screenOptions={{ headerShown: false }}>
                  <Screen name="Landing" component={ Landing }/>
+                 <Screen name="GiveClasses" component={ GiveClasses }></Screen>
+                 <Screen name="Study" component={ StudyTabs }/>
              </Navigator>
          </NavigationContainer>
      );     
